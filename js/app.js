fdb046bf4cca225159cb0fbc7883b97bcf3e0892
elenah <elenah@interlink-ua.com>
2016-09-13 18:47:22 +0300

    1

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
-// 'use strict';
-angular.module('myApp',[]);
-    // .config(function ($routeProvider) {
-    //
-    // });
+'use strict';
+angular.module("myApp", ["ngRoute"])
+    .config( function ($routeProvider) {
+        $routeProvider
+            .when("/", {
+                templateUrl : "login.html"
+            });
+            // .when("/1", {
+            //     templateUrl : "login.html"
+            // })
+    });
